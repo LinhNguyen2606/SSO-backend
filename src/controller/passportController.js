0b478ed278b8900165c4938251961bd3740277d5
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2023-09-17 20:00:57 +0700

    Setup Passport for Nodejs

---
+++ src/controller/passportController.js
@@ -0,0 +1,25 @@
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import loginRegisterService from "../service/loginRegisterService";
+
+const configPassportLocal = () => {
+  passport.use(
+    new LocalStrategy(async function verify(username, password, cb) {
+      let rawData = {
+        valueLogin: username,
+        password: password,
+      };
+
+      let res = await loginRegisterService.handleUserLogin(rawData);
+      if (res && res === 0) {
+        return cb(null, res.DT);
+      } else {
+        return cb(null, false, { message: res.EM });
+      }
+    })
+  );
+};
+
+module.exports = {
+  configPassportLocal,
+};
